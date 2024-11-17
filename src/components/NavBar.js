5030f0f1d5b8ac24859d2d1258f230f466205a25
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-11-17 20:23:14 +0000

    Navbar and favicon added

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+
+const NavBar = () => {
+	return (
+		<Navbar expand="md" fixed="top">
+			<Container>
+				<Navbar.Brand>ChorePlanner</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ml-auto text-left">
+						<Nav.Link>Noticeboard <i class="fa-solid fa-check"></i></Nav.Link>
+						<Nav.Link>Your Home <i class="fa-solid fa-house"></i></Nav.Link>
+						<Nav.Link>Log in <i class="fa-solid fa-arrow-right-to-bracket"></i></Nav.Link>
+						<Nav.Link>Join <i class="fa-solid fa-user-plus"></i></Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+};
+
+export default NavBar;
