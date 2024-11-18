50ca44b2d118647c18cd0d713b52ea8da43d7351
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-11-18 13:27:42 +0000

    Routes set up in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,46 @@
 import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
-import styles from '../styles/NavBar.module.css'
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
 	return (
 		<Navbar className={styles.NavBar} expand="md" fixed="top">
 			<Container>
-				<Navbar.Brand className={styles.Brand}>ChorePlanner</Navbar.Brand>
+				<NavLink exact to="/">
+					<Navbar.Brand className={styles.Brand}>ChorePlanner</Navbar.Brand>
+				</NavLink>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ml-auto text-left">
-						<Nav.Link>Noticeboard <i class="fa-solid fa-check"></i></Nav.Link>
-						<Nav.Link>Your Home <i class="fa-solid fa-house"></i></Nav.Link>
-						<Nav.Link>Log in <i class="fa-solid fa-arrow-right-to-bracket"></i></Nav.Link>
-						<Nav.Link>Join <i class="fa-solid fa-user-plus"></i></Nav.Link>
+						<NavLink
+							className={styles.NavLink}
+							activeClassName={styles.Active}
+							to="/noticeboard"
+						>
+							Noticeboard <i class="fa-solid fa-check"></i>
+						</NavLink>
+						<NavLink
+							className={styles.NavLink}
+							activeClassName={styles.Active}
+							to="/households"
+						>
+							Your Home <i class="fa-solid fa-house"></i>
+						</NavLink>
+						<NavLink
+							className={styles.NavLink}
+							activeClassName={styles.Active}
+							to="/signin"
+						>
+							Log in <i class="fa-solid fa-arrow-right-to-bracket"></i>
+						</NavLink>
+						<NavLink
+							className={styles.NavLink}
+							activeClassName={styles.Active}
+							to="/signup"
+						>
+							Join <i class="fa-solid fa-user-plus"></i>
+						</NavLink>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
