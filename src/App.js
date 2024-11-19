0e5e13f860a25b73f9b02dc61bbb2a7031be7ca8
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-11-19 11:01:38 +0000

    Sign in form added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import { Route,Switch } from 'react-router-dom';
 import './api/axiosDefault'
 import SignUpForm from './pages/auth/SignUpForm';
+import SignInForm from './pages/auth/SignInForm';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
           <Route exact path='/' render={()=> <h1>Home Page</h1>} />
           <Route exact path='/noticeboard' render={()=> <h1>Noticeboard</h1>} />
           <Route exact path='/households' render={()=> <h1>Your Home</h1>} />
-          <Route exact path='/signin' render={()=> <h1>Sign In</h1>} />
+          <Route exact path='/signin' render={()=> <SignInForm />} />
           <Route exact path='/signup' render={()=> <SignUpForm />} />
           <Route render={()=> <h1>Page not found!</h1>} />
         </Switch>
---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+
+import { Form, Button, Col, Row, Container, Alert } from "react-bootstrap";
+import axios from "axios";
+
+const SignInForm = () => {
+	const [signInData, setSignInData] = useState({
+		username: "",
+		password: "",
+		
+	});
+	const { username, password } = signInData;
+
+	const [errors, setErrors] = useState({});
+
+	const history = useHistory();
+
+	const handleChange = (event) => {
+		setSignInData({
+			...signInData,
+			[event.target.name]: event.target.value,
+		});
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			await axios.post("/dj-rest-auth/login/", signInData);
+			history.push("/");
+		} catch (err) {
+			setErrors(err.response?.data);
+		}
+	};
+	return (
+		<Row className={styles.Row}>
+			<Col className="my-auto py-2 p-md-2" md={6}>
+				<Container className={`${appStyles.Content} p-4 `}>
+					<h1 className={styles.Header}>Sign in</h1>
+
+					<Form onSubmit={handleSubmit}>
+						<Form.Group controlId="username">
+							<Form.Label className="d-none">username</Form.Label>
+							<Form.Control
+								className={styles.Input}
+								type="text"
+								placeholder="Enter username"
+								name="username"
+								value={username}
+								onChange={handleChange}
+							/>
+						</Form.Group>
+						{errors.username?.map((message, idx) => (
+							<Alert variant="warning" key={idx}>
+								{message}
+							</Alert>
+						))}
+
+						<Form.Group controlId="password">
+							<Form.Label className="d-none">Password</Form.Label>
+							<Form.Control
+								className={styles.Input}
+								type="password"
+								placeholder="Password"
+								name="password"
+								value={password}
+								onChange={handleChange}
+							/>
+						</Form.Group>
+						{errors.password?.map((message, idx) => (
+							<Alert variant="warning" key={idx}>
+								{message}
+							</Alert>
+						))}
+						
+
+						<Button
+							className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Pink}`}
+							type="submit"
+						>
+							Sign In
+						</Button>
+						{errors.non_field_errors?.map((message, idx) => (
+							<Alert variant="warning" key={idx} className="mt-3">
+								{message}
+							</Alert>
+						))}
+					</Form>
+				</Container>
+				<Container className={`mt-3 ${appStyles.Content}`}>
+					<Link className={styles.Link} to="/signup">
+						Not a member yet? <span>Sign up here</span>
+					</Link>
+				</Container>
+			</Col>
+		</Row>
+	);
+};
+
+export default SignInForm;
