e137a13afe4f00a98d7719b3c529d3a1dde9168c
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-11-19 14:35:47 +0000

    Added hook to handle toggle of the burger in the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,10 +8,13 @@ import {
 } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import UseClickOutsideToggle from "../hooks/UseClickOutsideToggle";
 
 const NavBar = () => {
 	const currentUser = useCurrentUser();
 	const setCurrentUser = useSetCurrentUser();
+
+	const {expanded, setExpanded, ref } = UseClickOutsideToggle()
 
 	const handleSignOut = async () => {
 		try {
@@ -69,12 +72,21 @@ const NavBar = () => {
 	);
 
 	return (
-		<Navbar className={styles.NavBar} expand="md" fixed="top">
+		<Navbar
+			expanded={expanded}
+			className={styles.NavBar}
+			expand="md"
+			fixed="top"
+		>
 			<Container>
 				<NavLink exact to="/">
 					<Navbar.Brand className={styles.Brand}>ChorePlanner</Navbar.Brand>
 				</NavLink>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Toggle
+					ref={ref}
+					onClick={() => setExpanded(!expanded)}
+					aria-controls="basic-navbar-nav"
+				/>
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ml-auto text-left">
 						{currentUser ? loggedInIcons : loggedOutIcons}
