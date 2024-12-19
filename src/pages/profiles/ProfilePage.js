fc0cbb3c8b2866555b1a675cd8f697d0a1e2890a
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-12-19 20:15:26 +0000

    Added loading spinner to the profilepage

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Spinner } from "react-bootstrap";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefault";
 import ProfileDetail from "./ProfileDetail";
+import taskStyles from "../../styles/Task.module.css";
+import taskPageStyles from "../../styles/TasksPage.module.css";
 
 function ProfilePage() {
 	const { id } = useParams();
 	const [profile, setProfile] = useState({ results: [] });
+	const [hasLoaded, setHasLoaded] = useState(false);
 
 	useEffect(() => {
 		const handleMount = async () => {
@@ -16,21 +19,36 @@ function ProfilePage() {
 					axiosReq.get(`/profiles/${id}`),
 				]);
 				setProfile({ results: [profile] });
-				console.log(profile);
+				setHasLoaded(true);
 			} catch (err) {
 				console.log(err);
 			}
 		};
-		handleMount();
+		setHasLoaded(false);
+		const timer = setTimeout(() => {
+			handleMount();
+		}, 1000);
+		return () => {
+			clearTimeout(timer);
+		};
 	}, [id]);
 
-	return (
+	return hasLoaded ? (
 		<Container fluid className={appStyles.Content}>
 			<ProfileDetail
 				{...profile.results[0]}
 				setProfile={setProfile}
 				profilePage
 			/>
+		</Container>
+	) : (
+		<Container
+			fluid
+			className={`${appStyles.Content}  ${taskStyles.Text} ${taskPageStyles.Spinner}`}
+		>
+			<Spinner animation="border" role="status">
+				<span className="sr-only">Loading...</span>
+			</Spinner>
 		</Container>
 	);
 }
