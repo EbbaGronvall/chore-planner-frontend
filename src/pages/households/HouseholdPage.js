e986dbf346f0c8285b2a1ca8a925db5654f1887a
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-12-19 20:12:07 +0000

    Added loading spinner to householdpage for improved ux

--- src/pages/households/HouseholdPage.js
+++ src/pages/households/HouseholdPage.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Spinner } from "react-bootstrap";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefault";
 import HouseholdDetail from "./HouseholdDetail";
+import taskStyles from "../../styles/Task.module.css";
+import taskPageStyles from "../../styles/TasksPage.module.css";
 
 function HouseholdPage() {
 	const { slug } = useParams();
 	const [household, setHousehold] = useState({ results: [] });
+	const [hasLoaded, setHasLoaded] = useState(false);
 
 	useEffect(() => {
 		const handleMount = async () => {
@@ -16,21 +19,36 @@ function HouseholdPage() {
 					axiosReq.get(`/households/${slug}`),
 				]);
 				setHousehold({ results: [household] });
-				console.log(household);
+				setHasLoaded(true);
 			} catch (err) {
 				console.log(err);
 			}
 		};
-		handleMount();
+		setHasLoaded(false);
+		const timer = setTimeout(() => {
+			handleMount();
+		}, 1000);
+		return () => {
+			clearTimeout(timer);
+		};
 	}, [slug]);
 
-	return (
+	return hasLoaded ? (
 		<Container fluid className={appStyles.Content}>
 			<HouseholdDetail
 				{...household.results[0]}
 				setHousehold={setHousehold}
 				householdPage
 			/>
+		</Container>
+	) : (
+		<Container
+			fluid
+			className={`${appStyles.Content}  ${taskStyles.Text} ${taskPageStyles.Spinner}`}
+		>
+			<Spinner animation="border" role="status">
+				<span className="sr-only">Loading...</span>
+			</Spinner>
 		</Container>
 	);
 }
